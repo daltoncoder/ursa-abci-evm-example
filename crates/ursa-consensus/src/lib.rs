b330cce813d158703dbed2682b346cd8cb78ec07
daltoncoder <71679972+daltoncoder@users.noreply.github.com>
2023-01-16 18:47:00 -0500

    feat: integrated https://github.com/gakonst/narwhal-abci-evm

---
+++ crates/ursa-consensus/src/lib.rs
@@ -0,0 +1,17 @@
+mod engine;
+pub use engine::Engine;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct BroadcastTxQuery {
+    tx: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct AbciQueryQuery {
+    path: String,
+    data: String,
+    height: Option<usize>,
+    prove: Option<bool>,
+}
