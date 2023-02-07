c03f10e869cab33a6bee5d8de0751919f52193db
daltoncoder <daltoncoder2@gmail.com>
2023-02-07 17:06:09 -0500

    chore: bumped revm to 3.0.0

--- crates/ursa-application/src/types.rs
+++ crates/ursa-application/src/types.rs
@@ -1,8 +1,4 @@
-use anyhow::Result;
-use ethers::prelude::*;
-use std::sync::Arc;
-use tokio::sync::Mutex;
-
+use anyhow::{Result, bail};
 use abci::{
     async_api::{
         Consensus as ConsensusTrait, Info as InfoTrait, Mempool as MempoolTrait,
@@ -11,16 +7,13 @@ use abci::{
     async_trait,
     types::*,
 };
+use revm::{self, Database, DatabaseCommit, db::{CacheDB, EmptyDB}, primitives::{Env, TxEnv, EVMResult, ExecutionResult}};
+use std::sync::{Arc};
+use tokio::sync::Mutex;
+use ethers::prelude::{NameOrAddress};
+use ethers::types::TransactionRequest;
+use revm::primitives::{Address, CreateScheme, TransactTo, U256};
 
-use revm::{
-    self,
-    db::{CacheDB, EmptyDB},
-    CreateScheme, Database, DatabaseCommit, Env, Log as RevmLog, Return, TransactOut, TransactTo,
-    TxEnv,
-};
-
-/// The app's state, containing a Revm DB.
-// TODO: Should we instead try to replace this with Anvil and implement traits for it?
 #[derive(Clone, Debug)]
 pub struct State<Db> {
     pub block_height: i64,
@@ -40,53 +33,40 @@ impl Default for State<CacheDB<EmptyDB>> {
     }
 }
 
-#[derive(serde::Serialize, serde::Deserialize, Debug)]
-pub struct TransactionResult {
-    transaction: TransactionRequest,
-    exit: Return,
-    out: TransactOut,
-    gas: u64,
-    logs: Vec<RevmLog>,
-}
-
 impl<Db: Database + DatabaseCommit> State<Db> {
     async fn execute(
         &mut self,
         tx: TransactionRequest,
         read_only: bool,
-    ) -> Result<TransactionResult> {
+    ) -> Result<ExecutionResult> {
         let mut evm = revm::EVM::new();
         evm.env = self.env.clone();
         evm.env.tx = TxEnv {
-            caller: tx.from.unwrap_or_default(),
+            caller: tx.from.unwrap_or_default().to_fixed_bytes().into(),
             transact_to: match tx.to {
-                Some(NameOrAddress::Address(inner)) => TransactTo::Call(inner),
-                Some(NameOrAddress::Name(_)) => panic!("not allowed"),
+                Some(NameOrAddress::Address(inner)) => TransactTo::Call(inner.to_fixed_bytes().into()),
+                Some(NameOrAddress::Name(_)) => bail!("not allowed"),
                 None => TransactTo::Create(CreateScheme::Create),
             },
             data: tx.data.clone().unwrap_or_default().0,
-            chain_id: Some(self.env.cfg.chain_id.as_u64()),
+            chain_id: Some(self.env.cfg.chain_id.try_into().unwrap()),
             nonce: Some(tx.nonce.unwrap_or_default().as_u64()),
-            value: tx.value.unwrap_or_default(),
-            gas_price: tx.gas_price.unwrap_or_default(),
-            gas_priority_fee: Some(tx.gas_price.unwrap_or_default()),
+            value: tx.value.unwrap_or_default().into(),
+            gas_price: tx.gas_price.unwrap_or_default().into(),
+            gas_priority_fee: Some(tx.gas_price.unwrap_or_default().into()),
             gas_limit: tx.gas.unwrap_or_default().as_u64(),
             access_list: vec![],
         };
         evm.database(&mut self.db);
 
-        let (execution_result, state) = evm.transact();
-        if !read_only {
-            self.db.commit(state);
+        let results = match evm.transact() {
+            Ok(data) => data,
+            _ => bail!("This is an error")
         };
-
-        Ok(TransactionResult {
-            transaction: tx,
-            exit: execution_result.exit_reason,
-            gas: execution_result.gas_used,
-            logs: execution_result.logs,
-            out: execution_result.out,
-        })
+        if !read_only {
+            self.db.commit(results.state);
+        };
+        Ok(results.result)
     }
 }
 
@@ -201,12 +181,12 @@ pub enum Query {
 #[derive(serde::Serialize, serde::Deserialize, Debug)]
 #[allow(clippy::large_enum_variant)]
 pub enum QueryResponse {
-    Tx(TransactionResult),
+    Tx(ExecutionResult),
     Balance(U256),
 }
 
 impl QueryResponse {
-    pub fn as_tx(&self) -> &TransactionResult {
+    pub fn as_tx(&self) -> &ExecutionResult {
         match self {
             QueryResponse::Tx(inner) => inner,
             _ => panic!("not a tx"),
@@ -248,8 +228,8 @@ impl<Db: Send + Sync + Database + DatabaseCommit> InfoTrait for Info<Db> {
                 let result = state.execute(tx, true).await.unwrap();
                 QueryResponse::Tx(result)
             }
-            Query::Balance(address) => match state.db.basic(address) {
-                Ok(info) => QueryResponse::Balance(info.unwrap_or_default().balance),
+            Query::Balance(address) => match state.db.basic(address.into()) {
+                Ok(info) => QueryResponse::Balance(info.unwrap_or_default().balance.into()),
                 _ => panic!("error retrieveing balance"),
             },
         };
@@ -279,61 +259,3 @@ pub struct Snapshot;
 
 impl SnapshotTrait for Snapshot {}
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    // use ethers::prelude::*;
-
-    #[tokio::test]
-    async fn run_and_query_tx() {
-        let val = ethers::utils::parse_units(1, 18).unwrap().into();
-        let alice = Address::random();
-        let bob = Address::random();
-
-        let mut state = State::default();
-
-        // give alice some money
-        state.db.insert_account_info(
-            alice,
-            revm::AccountInfo {
-                balance: val,
-                ..Default::default()
-            },
-        );
-
-        // make the tx
-        let tx = TransactionRequest::new()
-            .from(alice)
-            .to(bob)
-            .gas_price(0)
-            .data(vec![1, 2, 3, 4, 5])
-            .gas(31000)
-            .value(val);
-
-        // Send it over an ABCI message
-
-        let consensus = Consensus::new(state);
-
-        let req = RequestDeliverTx {
-            tx: serde_json::to_vec(&tx).unwrap(),
-        };
-        let res = consensus.deliver_tx(req).await;
-        let res: TransactionResult = serde_json::from_slice(&res.data).unwrap();
-        // tx passed
-        assert_eq!(res.exit, Return::Stop);
-
-        // now we query the state for bob's balance
-        let info = Info {
-            state: consensus.current_state.clone(),
-        };
-        let res = info
-            .query(RequestQuery {
-                data: serde_json::to_vec(&Query::Balance(bob)).unwrap(),
-                ..Default::default()
-            })
-            .await;
-        let res: QueryResponse = serde_json::from_slice(&res.value).unwrap();
-        let balance = res.as_balance();
-        assert_eq!(balance, val);
-    }
-}
